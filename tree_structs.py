bdcaa3a13c96ef1182cf74eda0ca08b1777e897b
lenacoll <lena.collienne@gmail.com>
2021-08-10 16:38:50 +1200

    del undefined function

--- tree_structs.py
+++ tree_structs.py
@@ -46,11 +46,6 @@ findpath_path = lib.return_findpath
 findpath_path.argtypes = [POINTER(TREE), POINTER(TREE)]
 findpath_path.restype = TREE_LIST
 
-# C function findpath_moves_per_iteration
-findpath_moves_per_iteration = lib.findpath_moves_per_iteration
-findpath_moves_per_iteration.argtypes = [POINTER(TREE),POINTER(TREE)]
-findpath_moves_per_iteration.restype = POINTER(c_long)
-
 # C function random_walk
 random_walk = lib.random_walk
 random_walk.argtypes = [POINTER(TREE), c_long]
