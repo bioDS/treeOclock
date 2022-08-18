de83bc5a86dbb89c6912f5e45185c18ff900c10a
Lena Collienne <lena@lenacoll.de>
2022-08-19 10:46:26 +1200

    add unlabelled SPR function to header

--- spr.h
+++ spr.h
@@ -5,10 +5,12 @@
 #include "queue.h"
 
 int spr_move(Tree * input_tree, long r, long new_sibling, int child_moving);
+int unlabelled_spr_move(Tree * input_tree, long r, long new_parent, int child_moving, int new_child_index)
 
 Tree_List all_spr_neighbourhood(Tree *input_tree, int horizontal);
 Tree_List spr_neighbourhood(Tree* input_tree);
 Tree_List hspr_neighbourhood(Tree* input_tree);
+Tree_List unlabelled_spr_neighbourhood(Tree* input_tree, int horizontal);
 
 Tree_List rankedspr_path_mrca_cluster_diff(Tree* start_tree, Tree* dest_tree, int hspr);
 Tree_List rankedspr_path_mrca_diff(Tree* start_tree, Tree* dest_tree, int hspr);
