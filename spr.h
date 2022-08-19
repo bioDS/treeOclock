933b8af260775c198e8028ff68ca739b2ad42750
Lena Collienne <lena@lenacoll.de>
2022-08-19 17:07:53 +1200

    typo

--- spr.h
+++ spr.h
@@ -5,7 +5,7 @@
 #include "queue.h"
 
 int spr_move(Tree * input_tree, long r, long new_sibling, int child_moving);
-int unlabelled_spr_move(Tree * input_tree, long r, long new_parent, int child_moving, int new_child_index)
+int unlabelled_spr_move(Tree * input_tree, long r, long new_parent, int child_moving, int new_child_index);
 
 Tree_List all_spr_neighbourhood(Tree *input_tree, int horizontal);
 Tree_List spr_neighbourhood(Tree* input_tree);
