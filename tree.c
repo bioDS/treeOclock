d7a25280004ce9c34ab907d5abffeb8298e6d0a3
Lena Collienne <lena@lenacoll.de>
2022-09-10 16:59:17 +1200

    function computing clusters + using variable for number of nodes

--- tree.c
+++ tree.c
@@ -14,11 +14,12 @@ Node empty_node(){
 
 
 Tree* empty_tree(long num_leaves){
-    // initalise tree on num_leaves leaves
+    // initialise tree on num_leaves leaves
+    long num_nodes = 2 * num_leaves - 1;
     Tree* new_tree = malloc(sizeof(Tree));
-    new_tree->tree = calloc((2 * num_leaves - 1), sizeof(Node));
+    new_tree->tree = calloc(num_nodes, sizeof(Node));
     new_tree->num_leaves = num_leaves;
-    for (long i = 0; i < 2 * num_leaves - 1; i++){
+    for (long i = 0; i < num_nodes; i++){
         new_tree->tree[i] = empty_node();
     }
     return new_tree;
@@ -28,8 +29,9 @@ Tree* empty_tree(long num_leaves){
 Tree* deep_copy(Tree* tree){
     // deep copy tree
     long num_leaves = tree->num_leaves;
+    long num_nodes = 2 * num_leaves - 1;
     Tree* tree_copy = empty_tree(num_leaves);
-    for (long i = 0; i < 2 * num_leaves - 1; i++){
+    for (long i = 0; i < num_nodes; i++){
         tree_copy->tree[i] = tree->tree[i];
     }
     return tree_copy;
@@ -46,10 +48,11 @@ void print_tree(Tree* tree){
 }
 
 
-// Check whether two trees have the same (ranked) tree topology -- return 0 if this is the case, 1 otherwise
+// Check whether two trees have the same (ranked) tree topology
 int same_topology(Tree* tree1, Tree* tree2){
     long num_leaves = tree1->num_leaves;
-    for(long i=num_leaves; i < 2 * num_leaves - 1; i++){
+    long num_nodes = 2 * num_leaves - 1;
+    for(long i = num_leaves; i < num_nodes; i++){
         if (tree1->tree[i].parent != tree2->tree[i].parent){
             return FALSE;
         }
@@ -58,9 +61,10 @@ int same_topology(Tree* tree1, Tree* tree2){
 }
 
 
-// Check whether two trees are identical (output 0 if this is true, 1 otherwise)
+// Check whether two trees are identical
 int same_tree(Tree* tree1, Tree* tree2){
-    for (long i = 0; i < 2* tree1->num_leaves - 1; i++){
+    long num_nodes = 2 * tree1->num_leaves - 1;
+    for (long i = 0; i < num_nodes; i++){
         if (tree1->tree[i].parent != tree2->tree[i].parent){
             return FALSE;
         }
@@ -68,8 +72,8 @@ int same_tree(Tree* tree1, Tree* tree2){
     return TRUE;
 }
 
-// find mrca of nodes with positions node1 and node2 in tree
-// returns the rank of the mrca
+
+// find rank of mrca of nodes with positions node1 and node2 in tree
 long mrca(Tree * input_tree, long node1, long node2){
     long rank1 = node1;
     long rank2 = node2;
@@ -77,11 +81,13 @@ long mrca(Tree * input_tree, long node1, long node2){
         if (rank1 < rank2){
             rank1 = input_tree->tree[rank1].parent;
             if (rank1 == -1){
+                printf("Cannot find mrca, reached root.");
                 return -1;
             }
         } else{
             rank2 = input_tree->tree[rank2].parent;
             if (rank2 == -1){
+                printf("Cannot find mrca, reached root.");
                 return -1;
             }
         }
@@ -90,51 +96,76 @@ long mrca(Tree * input_tree, long node1, long node2){
 }
 
 
-long* mrca_list(Tree* tree1, Tree* tree2){
+long* mrca_array(Tree* tree1, Tree* tree2){
+    // return array with rank(mrca_{tree1}(C_i)) at position i where C_i is the cluster induced by node of rank i in tree2
     long num_leaves = tree1->num_leaves;
-    long *mrca_list = malloc((2*num_leaves-1)*sizeof(long)); // at position i save rank(mrca_{tree1}(C_i)) where C_i is the cluster induced by node of rank i in tree2
+    long num_nodes = 2 * num_leaves - 1;
+    long *mrca_array = calloc(num_nodes, sizeof(long));
 
-    for (long i = num_leaves; i < 2*num_leaves - 1; i++){
-        // iterate through the ranks of mrcas in dest_tree
-        // find mrca (distinguish leaves vs. non-leaf and fill mrca_list to get mrcas of non-leafs)
+    for (long i = num_leaves; i < num_nodes; i++){
+        // find nodes (child0, child1) in tree1 that are mrcas of the clusters induced by children of node of rank i in tree2
         long child0;
         if (tree2->tree[i].children[0] < num_leaves){
             child0 = tree2->tree[i].children[0];
         } else{
-            child0 = mrca_list[tree2->tree[i].children[0]];
+            child0 = mrca_array[tree2->tree[i].children[0]];
         }
 
         long child1;
         if (tree2->tree[i].children[1] < num_leaves){
             child1 = tree2->tree[i].children[1];
         } else{
-            child1 = mrca_list[tree2->tree[i].children[1]];
+            child1 = mrca_array[tree2->tree[i].children[1]];
         }
 
         long current_mrca = mrca(tree1, child0, child1);
-        mrca_list[i] = current_mrca;
+        mrca_array[i] = current_mrca;
     }
-    return mrca_list;
+    return mrca_array;
 }
 
 
-long mrca_differences(Tree* current_tree, Tree* dest_tree, int include_leaf_parents){
-    // Compute differences of ranks of mrcas of all cluster of dest_tree btw current_tree and dest_tree
+long mrca_differences(Tree* tree1, Tree* tree2, int include_leaf_parents){
+    // Compute differences of ranks of mrcas of all clusters of tree2 btw tree1 and tree2
     // Also add ranks of parents of leaves if include_leaf_parents == 0
     long sum = 0;
-    long num_leaves = dest_tree->num_leaves;
-    // long mrcas[2*num_leaves-1]; // at position i save rank(mrca_{current_tree}(C_i)) where C_i is the cluster induced by node of rank i in dest_tree
+    long num_leaves = tree2->num_leaves;
+    long num_nodes = 2 * num_leaves - 1;
     // First iterate through leaves
-    if (include_leaf_parents == 0){
+    if (include_leaf_parents == TRUE){
         for (long i = 0; i < num_leaves; i++){
-            sum += abs(current_tree->tree[i].parent - dest_tree->tree[i].parent);
+            sum += abs(tree1->tree[i].parent - tree2->tree[i].parent);
         }
     }
-    long* mrcas = mrca_list(current_tree, dest_tree);
-    for (long i = num_leaves; i < 2*num_leaves-1; i++){
-        sum += (mrcas[i] - i);
+    long* mrcas = mrca_list(tree1, tree2);
+    for (long i = num_leaves; i < num_nodes; i++){
+        sum += mrcas[i] - i;
     }
     return sum;
+}
+
+
+long ** get_clusters(Tree* tree){
+    // return matrix cluster*leaves -- 0 if leaf is not in cluster, 1 if it is in cluster
+    long num_leaves = tree->num_leaves;
+    long ** clusters = calloc(num_leaves - 1, sizeof(long *));
+    for (long i = 0; i < num_leaves - 1; i++){
+        clusters[i] = calloc(num_leaves, sizeof(long));
+    }
+
+    for (long i = 0; i < num_leaves ; i++){
+        for (long j = 0; j < num_leaves - 1; j++){
+            clusters[j][i] = 0; //initialise all entries to be 0
+        }
+        // fill clusters
+        long j = i;
+        while (tree->tree[j].parent != -1){
+            j = tree->tree[j].parent;
+            clusters[j - num_leaves][i] = 1;
+        }
+        clusters[num_leaves - 2][i] = 1;
+    }
+    return clusters;
 }
 
 
@@ -142,41 +173,8 @@ long sum_symmetric_cluster_diff(Tree* tree1, Tree* tree2){
     // Computes sum of symmetric differences of clusters of tree1 and tree2 for all ranks i=1,..,n-1
     long num_leaves = tree1->num_leaves;
 
-    // Get clusters for both trees:
-    // create matrix cluster*leaves -- 0 if leaf is not in cluster, 1 if it is in cluster
-    long ** clusters_t1 = malloc((num_leaves - 1) * sizeof(long *));
-    for (long i = 0; i < num_leaves - 1; i++){
-        clusters_t1[i] = malloc((num_leaves) * sizeof(long));
-    }
-
-    for (long i = 0; i < num_leaves ; i++){
-        for (long j = 0; j < num_leaves - 1; j++){
-            clusters_t1[j][i] = 0; //initialise all entries to be 0
-        }
-        long j = i;
-        while (tree1->tree[j].parent != -1){
-            j = tree1->tree[j].parent;
-            clusters_t1[j - num_leaves][i] = 1;
-        }
-        clusters_t1[num_leaves - 2][i] = 1;
-    }
-
-    long ** clusters_t2 = malloc((num_leaves - 1) * sizeof(long *));
-    for (long i = 0; i < num_leaves - 1; i++){
-        clusters_t2[i] = malloc((num_leaves) * sizeof(long));
-    }
-
-    for (long i = 0; i < num_leaves ; i++){
-        for (long j = 0; j < num_leaves - 1; j++){
-            clusters_t2[j][i] = 0; //initialise all entries to be 0
-        }
-        long j = i;
-        while (tree2->tree[j].parent != -1){
-            j = tree2->tree[j].parent;
-            clusters_t2[j - num_leaves][i] = 1;
-        }
-        clusters_t2[num_leaves - 2][i] = 1;
-    }
+    long ** clusters_t1 = get_clusters(tree1);
+    long ** clusters_t2 = get_clusters(tree2);
 
     // Now compute symmetric difference between clusters (i.e. all columns that have a 1 in cluster_t1 or cluster_t2 and a 0 in the other matrix)
     long symm_diff = 0;
@@ -193,46 +191,11 @@ long sum_symmetric_cluster_diff(Tree* tree1, Tree* tree2){
 }
 
 
-// Compute symmetric difference of clusters induced by nodes of rank k in tree1 and tree2
 long symmetric_cluster_diff(Tree* tree1, Tree* tree2, long k){
+    // Compute symmetric difference of clusters induced by nodes of rank k in tree1 and tree2
     long num_leaves = tree1->num_leaves;
-    // find clusters induced by node of rank k in both tree1 and tree2
-    // Note: getting cluster matrix is copy&paste from tree_to_string function. We only need one column of this matrix here
-    // create matrix cluster*leaves -- 0 if leaf is not in cluster, 1 if it is in cluster
-    // first: clusters_t1 for tree1
-    long ** clusters_t1 = malloc((num_leaves - 1) * sizeof(long *));
-    for (long i = 0; i < num_leaves - 1; i++){
-        clusters_t1[i] = malloc((num_leaves) * sizeof(long));
-    }
-
-    for (long i = 0; i < num_leaves ; i++){
-        for (long j = 0; j < num_leaves - 1; j++){
-            clusters_t1[j][i] = 0; //initialise all entries to be 0
-        }
-        long j = i;
-        while (tree1->tree[j].parent != -1){
-            j = tree1->tree[j].parent;
-            clusters_t1[j - num_leaves][i] = 1;
-        }
-        clusters_t1[num_leaves - 2][i] = 1;
-    }
-    // same thing for tree2
-    long ** clusters_t2 = malloc((num_leaves - 1) * sizeof(long *));
-    for (long i = 0; i < num_leaves - 1; i++){
-        clusters_t2[i] = malloc((num_leaves) * sizeof(long));
-    }
-
-    for (long i = 0; i < num_leaves ; i++){
-        for (long j = 0; j < num_leaves - 1; j++){
-            clusters_t2[j][i] = 0; //initialise all entries to be 0
-        }
-        long j = i;
-        while (tree2->tree[j].parent != -1){
-            j = tree2->tree[j].parent;
-            clusters_t2[j - num_leaves][i] = 1;
-        }
-        clusters_t2[num_leaves - 2][i] = 1;
-    }
+    long ** clusters_t1 = get_clusters(tree1);
+    long ** clusters_t2 = get_clusters(tree2);
 
     // now count the number of entries that are a 1 in either cluster_t1 or cluster_t2 in row k-num_leaves
     long output = 0;
