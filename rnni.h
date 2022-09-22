db887c05c765258c7ce74358eac37c5f5b254733
Lena Collienne <lena@lenacoll.de>
2022-09-22 14:59:03 +1200

    explain functionss in rnni.h

--- rnni.h
+++ rnni.h
@@ -8,18 +8,30 @@ typedef struct Path {
     long length;
 } Path;
 
-int nni_move(Tree* input_tree, long r, int child_moves_up);
-int rank_move(Tree* input_tree, long r);
-int move_up(Tree* itree, long i, long k);
+// NNI move on edge [r,r+1] moving children[0] of r up to be child of r+1
+int nni_move(Tree* tree, long r, int child_moves_up);
+// rank move swapping ranks of nodes r and r+1
+int rank_move(Tree* tree, long r);
+// length moves moving all nodes between lowest_moving_node and k up by length
+// move so that all those nodes are above k in returned tree
+int move_up(Tree* tree, long lowest_moving_node, long k);
 
-Tree_Array rnni_neighbourhood(Tree* input_tree);
-Tree_Array rank_neighbourhood(Tree* input_tree);
+// returns all rnni neighbours of tree
+Tree_Array rnni_neighbourhood(Tree* tree);
+// returns all trees resulting from rank moves on tree
+Tree_Array rank_neighbourhood(Tree* tree);
+// returns one neighbour drawn uniformly from one-neighbourhood
 int uniform_neighbour(Tree* input_tree);
 
+// performs (unique) RNNI move on tree that decreases the rank of the most
+// recent common ancestor of node1 and node2 by one
 int decrease_mrca(Tree* tree, long node1, long node2);
 
+// computes a Path encoding all moves done on the FindPath path from start_tree
+// to dest_tree
 Path findpath_moves(Tree* start_tree, Tree* dest_tree);
 long rnni_distance(Tree* start_tree, Tree* dest_tree);
+// Returns all trees along FindPath path from start_tree to dest_tree
 Tree_Array findpath(Tree* start_tree, Tree* dest_tree);
 
 #endif
